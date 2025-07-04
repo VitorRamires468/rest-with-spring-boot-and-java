a959b4b2d8a551eb87c8864d4d41fe1e925905bc
vitorramires468 <ramiresvitor468@gmail.com>
2025-07-03 21:41:56 -0300

    Rest parameters and exception handler

---
+++ 05_ParameterAndExceptionHandler/rest-with-spring-boot-and-java/src/main/java/br/com/vitorramires468/controllers/GreetingController.java
@@ -0,0 +1,24 @@
+package br.com.vitorramires468.controllers;
+
+import br.com.vitorramires468.exceptions.ExceptionResponse;
+import br.com.vitorramires468.model.Greeting;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+}
---
+++ 05_ParameterAndExceptionHandler/rest-with-spring-boot-and-java/src/main/java/br/com/vitorramires468/model/Greeting.java
@@ -0,0 +1,7 @@
+package br.com.vitorramires468.model;
+
+public record Greeting(
+        Long id,
+        String content
+) {
+}
