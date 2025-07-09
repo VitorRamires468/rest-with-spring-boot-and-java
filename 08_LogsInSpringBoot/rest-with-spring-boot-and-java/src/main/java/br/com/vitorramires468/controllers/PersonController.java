25fb53d2abe44b9266660cc09976752ce3c4b234
vitorramires468 <ramiresvitor468@gmail.com>
2025-07-08 21:51:14 -0300

    Using logs

---
+++ 08_LogsInSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/vitorramires468/controllers/PersonController.java
@@ -0,0 +1,45 @@
+package br.com.vitorramires468.controllers;
+
+import br.com.vitorramires468.data.dto.PersonDTO;
+import br.com.vitorramires468.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices services;
+
+    @GetMapping
+    public List<PersonDTO> findAll(){
+        return services.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public PersonDTO findById(@PathVariable Long id){
+        return services.findById(id);
+    }
+
+    @PostMapping
+    public PersonDTO create(@RequestBody PersonDTO person){
+        return services.create(person);
+    }
+
+    @PutMapping
+    public PersonDTO update(@RequestBody PersonDTO person){
+        return services.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity delete(@PathVariable Long id){
+        services.delete(id);
+        return  ResponseEntity.noContent().build();
+    }
+
+}
