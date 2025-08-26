e171343ffe99bd07b6df1f454cb0e38fb01c5e88
vitorramires468 <ramiresvitor468@gmail.com>
2025-08-25 23:16:14 -0300

    Configuring Cross-Origin Resource Sharing (CORS)

---
+++ 18_ConfigurationCORS/rest-with-spring-boot-and-java/src/test/java/br/com/vitorramires468/config/TestConfigs.java
@@ -0,0 +1,11 @@
+package br.com.vitorramires468.config;
+
+public interface TestConfigs {
+    int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION =  "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+    String HEADER_PARAM_ERUDIO = "https://www.erudio.com.br";
+    String HEADER_PARAM_SEMERU = "https://www.semeru.com.br";
+    String HEADER_PARAM_LOCAL = "http://localhost:8080";
+}
